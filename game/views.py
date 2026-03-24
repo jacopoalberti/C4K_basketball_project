93eabad1683d47e8fca40d165760bc8a3d426429
jacopoalberti <jacopo.alberti33@gmail.com>
2026-03-24 14:11:49 -0400

    urls and views.py changes to macth ID's and new templates

--- game/views.py
+++ game/views.py
@@ -3,27 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .models import Player, Shot
+from .models import Player, Shot, Zone
+from django.http import JsonResponse
 
 def index(request):
     return HttpResponse("Hello, this is the game app!")
 
 def add_shot(request):
+    players = Player.objects.all()
+    zones = Zone.objects.all()
+
     if request.method == "POST":
         player_id = request.POST.get("player")
-        zone = request.POST.get("zone")
-        is_make = request.POST.get("result") == "make"
+        zone_id = request.POST.get("zone")
+        made = request.POST.get("result") == "make"
+        name = request.POST.get("name")
 
+        player = Player.objects.create(name=name)
         player = Player.objects.get(id=player_id)
 
         Shot.objects.create(
-            player=player,
-            zone=zone,
-            is_make=is_make
+            player_id=player_id,
+            zone_id=zone_id,
+            made=made
         )
 
-        return redirect("dashboard")
+        return JsonResponse({
+            "id": player.id,
+            "name": player.name
+        })
 
-    players = Player.objects.all()
-    return render(request, "add_shot.html", {"players": players})
+
+    return render(request, "add_shot.html", {
+        "players": players,
+        "zones": zones
+    })
